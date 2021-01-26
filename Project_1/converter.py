2c2419e3ad630ed3207a3476fbadbf29ac713cbb
YANYU ZHANG <zhangya@bu.edu>
2021-01-26 10:44:58 -0500

    Update converter.py

--- Project_1/converter.py
+++ Project_1/converter.py
@@ -1,1 +1,17 @@
+# ======================================
+# Example for EC500 A2 Project 1
+# Temperature C to/from F
+# ======================================
 
+import numpy as np
+
+# Fahrenheit = (Celsius * 9/5) + 32
+def c2f(c):
+  f = (c * 9 / 5) + 32
+  return round(f, 2) # Keep 2 decimals
+
+# Celsius = (Fahrenheit – 32) * 5/9
+def f2c(f):
+  c = (f - 32) * 5 / 9
+  return round(c, 2) # Keep 2 decimals
+  
