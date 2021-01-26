f5612de506bf32cee765617b4673e1de834e38ff
YANYU ZHANG <zhangya@bu.edu>
2021-01-26 11:03:40 -0500

    Update converter.py

--- Project_1/converter.py
+++ Project_1/converter.py
@@ -12,11 +12,17 @@ import numpy as np
 
 # Fahrenheit = (Celsius * 9/5) + 32
 def c2f(c):
+  if not isinstance(c, float):
+    return "ERROR -----> It is not a int"
+  
   f = (c * 9 / 5) + 32
   return round(f, 2) # Keep 2 decimals
 
 # Celsius = (Fahrenheit – 32) * 5/9
 def f2c(f):
+  if not isinstance(f, float):
+    return "ERROR -----> It is not a int"
+  
   c = (f - 32) * 5 / 9
   return round(c, 2) # Keep 2 decimals
   
