61f07010da7f5ffedf7f876d000b99b6c1751301
YANYU ZHANG <zhangya@bu.edu>
2021-01-26 11:02:13 -0500

    Update test_converter.py

--- Project_1/test_converter.py
+++ Project_1/test_converter.py
@@ -12,31 +12,17 @@ from converter import *
 import pytest
 
 def test_c2f():
-  pass
+  assert c2f(0.00) == 32.00
+  assert c2f(100.00) == 212.00
+  assert c2f(999.00) == 1830.20
+  
+	assert alabo2roman("String") == "ERROR -----> It is not a int"
+	assert alabo2roman("-123") == "ERROR -----> It is not a int"
+	assert alabo2roman("osama") == "ERROR -----> It is not a int"
+
   
 def test_f2c():
   pass
 
-# def test_right(): 
-# 	assert alabo2roman(1213) == "MCCXIII"
-# 	assert alabo2roman(20) == "XX"
-# 	assert alabo2roman(384) == "CCCLXXXIV"
-# 	assert alabo2roman(99) == "XCIX"
-# 	assert alabo2roman(999) == "CMXCIX"
-# 	assert alabo2roman(3000) == "MMM"
-# 	assert alabo2roman(100) == "C"
-# 	assert alabo2roman(3999) == "MMMCMXCIX"
-# 	assert alabo2roman(12) == "XII"
-# 	assert alabo2roman(432) == "CDXXXII"
 
-# def test_error():
-# 	assert alabo2roman("String") == "ERROR -----> It is not a int"
-# 	assert alabo2roman(23.4) == "ERROR -----> It is not a int" 
-# 	assert alabo2roman(-3.4) == "ERROR -----> It is not a int"
-# 	assert alabo2roman("-123") == "ERROR -----> It is not a int"
-# 	assert alabo2roman("osama") == "ERROR -----> It is not a int"
-# 	assert alabo2roman(123.5) == "ERROR -----> It is not a int"
-# 	assert alabo2roman(-1) == "ERROR -----> It is not in the range [1,3999]"
-# 	assert alabo2roman(0) == "ERROR -----> It is not in the range [1,3999]"
-# 	assert alabo2roman(4000) == "ERROR -----> It is not in the range [1,3999]"
-# 	assert alabo2roman(-400) == "ERROR -----> It is not in the range [1,3999]"
+
