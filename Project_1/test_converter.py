cd0f2aa9a92189f0bccb2080fd882fdeda0a21d3
YANYU ZHANG <zhangya@bu.edu>
2021-01-27 09:43:05 -0500

    Update test_converter.py

--- Project_1/test_converter.py
+++ Project_1/test_converter.py
@@ -13,9 +13,9 @@ import pytest
 
 def test_c2f():
   # assert some right cases
-  assert c2f(0.00) == 32.00
-  assert c2f(100.00) == 212.00
-  assert c2f(999.00) == 1830.20
+  assert c2f(0) == 32.00
+  assert c2f(100) == 212.00
+  assert c2f(999) == 1830.20
   # assert some error here
   assert c2f("String") == "ERROR -----> It is not a int"
   assert c2f("-123") == "ERROR -----> It is not a int"
